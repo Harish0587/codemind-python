2173da934ec4891f8584fdb6fe4254695f7c9648
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2022-05-22 22:13:18 +0530

    n=int(input()) s=0 f=0 a=list(map(int,input().strip().split()))[:n] for  i in range(n):     s=s+a[i] c=s//n for i in range(n):     if c==a[i]:          f=1          break if f==1:     print("True") else:     print("False")

--- average_element_is_in_array_or_not.py
+++ average_element_is_in_array_or_not.py
@@ -2,15 +2,14 @@ n=int(input())
 s=0
 f=0
 a=list(map(int,input().strip().split()))[:n]
-for i in range(n):
+for  i in range(n):
     s=s+a[i]
 c=s//n
 for i in range(n):
     if c==a[i]:
-        f=1
-        break
+         f=1
+         break
 if f==1:
     print("True")
 else:
     print("False")
-
