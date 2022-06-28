20e84a55d49a891c4cebce12782ec70da40741db
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2022-06-28 18:40:51 +0530

    n=int(input()) s=0 square=n*n while(square!=0):     r=square%10     s=s+r     square=square//10 if s==n:     print("Neon Number") else:     print("Not Neon Number")

--- Neon_Number.py
+++ Neon_Number.py
@@ -1,11 +1,11 @@
 n=int(input())
 s=0
-sq=n*n
-while(sq!=0):
-    r=sq%10
+square=n*n
+while(square!=0):
+    r=square%10
     s=s+r
-    sq=sq//10
-if(s==n):
+    square=square//10
+if s==n:
     print("Neon Number")
 else:
     print("Not Neon Number")
