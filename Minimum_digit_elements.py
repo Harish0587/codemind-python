861b52b0a22e8789f051aa2c0dee5f9350557e83
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2022-07-10 23:04:24 +0530

    n=int(input()) arr=list(map(int,input().split())) m=100 c=0 for i in arr:     if len(str(i))<m:         m=len(str(i)) for i in arr:     if len(str(i))==m:         c=c+1 print(c)

---
+++ Minimum_digit_elements.py
@@ -0,0 +1,11 @@
+n=int(input())
+arr=list(map(int,input().split()))
+m=100
+c=0
+for i in arr:
+    if len(str(i))<m:
+        m=len(str(i))
+for i in arr:
+    if len(str(i))==m:
+        c=c+1
+print(c)        
