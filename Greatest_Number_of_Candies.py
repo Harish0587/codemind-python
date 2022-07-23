ea25e0e61b44678d5548b0e8820c2625a79ab7eb
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2022-07-23 14:14:49 +0530

    n=int(input()) arr=list(map(int,input().split())) m=int(input()) x=max(arr) for i in arr:     if i+m>=x:         print("True",end=' ')     else:         print("False",end=' ')

---
+++ Greatest_Number_of_Candies.py
@@ -0,0 +1,9 @@
+n=int(input())
+arr=list(map(int,input().split()))
+m=int(input())
+x=max(arr)
+for i in arr:
+    if i+m>=x:
+        print("True",end=' ')
+    else:
+        print("False",end=' ')
