bb4dad881f0cf952f916747e6ac84fada738014a
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2022-07-23 13:31:19 +0530

    n=int(input()) for i in range(n):     m=int(input())     arr=list(map(int,input().split()))     l=[]     if m%2==0:         for i in range(m//2):             l.append(arr[m-i-1])             l.append(arr[i])     else:         for i in range(m//2):             l.append(arr[m-i-1])             l.append(arr[i])         l.append(arr[m//2])     print(*l)

---
+++ Rearrange_Array_Alternately.py
@@ -0,0 +1,15 @@
+n=int(input())
+for i in range(n):
+    m=int(input())
+    arr=list(map(int,input().split()))
+    l=[]
+    if m%2==0:
+        for i in range(m//2):
+            l.append(arr[m-i-1])
+            l.append(arr[i])
+    else:
+        for i in range(m//2):
+            l.append(arr[m-i-1])
+            l.append(arr[i])
+        l.append(arr[m//2])
+    print(*l)        
