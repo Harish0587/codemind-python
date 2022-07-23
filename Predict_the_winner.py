ebc785caa2bc5df4c84d16346650b91f9fdfca46
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2022-07-23 14:19:50 +0530

    n=int(input()) arr=list(map(int,input().split())) s=0 p=0 for i in range(n):     if i%2==0:         s=s+arr[i]     else:         p=p+arr[i] if(abs(s-p)%4==0):     print("X") else:     print("Y")

---
+++ Predict_the_winner.py
@@ -0,0 +1,13 @@
+n=int(input())
+arr=list(map(int,input().split()))
+s=0
+p=0
+for i in range(n):
+    if i%2==0:
+        s=s+arr[i]
+    else:
+        p=p+arr[i]
+if(abs(s-p)%4==0):
+    print("X")
+else:
+    print("Y")
