df9b497f1aa31aac06dd16d8cb52a16ba916b125
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2022-08-02 19:08:43 +0530

    n=input() n1=n.split() m=input() m1=m.split() l=[] k=[] c=0 for i in n1:     if n.count(i)==1:         l.append(i) for j in m1:     if m.count(j)==1:         k.append(j) for i in l:     if i in k:         c=c+1 print(c)

---
+++ Common_words_-2.py
@@ -0,0 +1,17 @@
+n=input()
+n1=n.split()
+m=input()
+m1=m.split()
+l=[]
+k=[]
+c=0
+for i in n1:
+    if n.count(i)==1:
+        l.append(i)
+for j in m1:
+    if m.count(j)==1:
+        k.append(j)
+for i in l:
+    if i in k:
+        c=c+1
+print(c)        
