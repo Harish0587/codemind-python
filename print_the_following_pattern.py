321813e5341823309ce1e6b79eb52ae04e4c1f14
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2022-10-03 20:00:21 +0530

    n=int(input()) ascii_value=65 for i in range(n):     for j in range(n):         alphabet=chr(ascii_value)         print(alphabet,end=" ")     ascii_value+=1     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,8 @@
 n=int(input())
+ascii_value=65
 for i in range(n):
-    for j in range(1,n-i+1):
-        print(j,end="")
-    print()    
+    for j in range(n):
+        alphabet=chr(ascii_value)
+        print(alphabet,end=" ")
+    ascii_value+=1 
+    print()
