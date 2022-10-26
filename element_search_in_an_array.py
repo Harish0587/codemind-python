3fd3cd19eb8a1d3e7d09447cba392c46df4f8c81
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2022-10-26 15:39:58 +0530

    n=int(input()) arr=list(map(int,input().split())) a=int(input()) f=0 for i in range(n):     if arr[i]==a:         f=1 if f==1:     print("True") else:     print("False")

--- element_search_in_an_array.py
+++ element_search_in_an_array.py
@@ -1,12 +1,11 @@
 n=int(input())
-arr=list(map(int,input().strip().split()))[:n]
-m=int(input())
+arr=list(map(int,input().split()))
+a=int(input())
 f=0
 for i in range(n):
-    if arr[i]==m:
+    if arr[i]==a:
         f=1
-        break
-if(f==1):
+if f==1:
     print("True")
 else:
     print("False")
