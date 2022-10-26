185a095b6e4554fad574961a29532fe5802650a8
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2022-10-26 15:56:27 +0530

    n=int(input()) arr=list(map(int,input().split())) c=[] s=[] for i in arr:     if(i%2!=0):         c.append(i)     else:         s.append(i) x=c+s print(*x)

--- array_of_add_and_even_elements.py
+++ array_of_add_and_even_elements.py
@@ -1,8 +1,11 @@
 n=int(input())
 arr=list(map(int,input().split()))
+c=[]
+s=[]
 for i in arr:
-    if i%2!=0:
-        print(i,end=" ")
-for i in arr:
-    if i%2==0:
-        print(i,end=" ")
+    if(i%2!=0):
+        c.append(i)
+    else:
+        s.append(i)
+x=c+s
+print(*x)
