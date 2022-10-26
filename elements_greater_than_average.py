031318f20d652cfc58daf57d4644e17a705689f0
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2022-10-26 20:50:50 +0530

    n=int(input()) arr=list(map(int,input().split())) c=0 s=0 for i in arr:     s=s+i avg=s//n for i in arr:     if i>=avg:         c+=1 print(c)

--- elements_greater_than_average.py
+++ elements_greater_than_average.py
@@ -1,11 +1,11 @@
 n=int(input())
 arr=list(map(int,input().split()))
-s=0
 c=0
-for i in range(n):
-    s=s+arr[i]
-a=s//n
-for i in range(n):
-    if arr[i]>=a:
-        c=c+1
+s=0
+for i in arr:
+    s=s+i
+avg=s//n
+for i in arr:
+    if i>=avg:
+        c+=1
 print(c)        
