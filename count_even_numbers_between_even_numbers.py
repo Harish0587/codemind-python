1f27396c97c250d995e01aa2ab7ce78f36dc0120
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2022-10-28 16:08:56 +0530

    n=int(input()) arr=list(map(int,input().split())) c=0 for i in range(1,n-1):     if arr[i-1]%2==0 and arr[i]%2==0 and arr[i+1]%2==0:         c=c+1 print(c)

--- count_even_numbers_between_even_numbers.py
+++ count_even_numbers_between_even_numbers.py
@@ -1,7 +1,7 @@
 n=int(input())
 arr=list(map(int,input().split()))
-s=0
+c=0
 for i in range(1,n-1):
-    if arr[i]%2==0 and arr[i-1]%2==0 and arr[i+1]%2==0:
-        s=s+1
-print(s)        
+    if arr[i-1]%2==0 and arr[i]%2==0 and arr[i+1]%2==0:
+        c=c+1
+print(c)
