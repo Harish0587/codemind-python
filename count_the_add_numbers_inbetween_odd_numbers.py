1604b927a3710e405af4c95abc506111097ce094
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2022-10-28 16:05:25 +0530

    n=int(input()) arr=list(map(int,input().split())) c=0 l=[] for i in range(1,n-1):     if arr[i]%2!=0 and arr[i+1]%2!=0 and arr[i-1]%2!=0:         c=c+1 print(c)

--- count_the_add_numbers_inbetween_odd_numbers.py
+++ count_the_add_numbers_inbetween_odd_numbers.py
@@ -1,8 +1,9 @@
 n=int(input())
 arr=list(map(int,input().split()))
 c=0
+l=[]
 for i in range(1,n-1):
-    if arr[i]%2!=0 and arr[i-1]%2!=0 and arr[i+1]%2!=0:
+    if arr[i]%2!=0 and arr[i+1]%2!=0 and arr[i-1]%2!=0:
         c=c+1
 print(c)        
-       
+    
