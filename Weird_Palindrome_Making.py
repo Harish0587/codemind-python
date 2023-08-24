16759eb210c40694fe0832ec96dd0f0ce904d005
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2023-08-24 22:03:57 +0530

    t=int(input()) for i in range(1,t+1):     x=int(input())     arr=list(map(int,input().split()))     c=0     for i in arr:         if i%2==1:             c+=1     print(c//2)

--- Weird_Palindrome_Making.py
+++ Weird_Palindrome_Making.py
@@ -1,9 +1,9 @@
 t=int(input())
 for i in range(1,t+1):
     x=int(input())
-    a=list(map(int,input().split()))
+    arr=list(map(int,input().split()))
     c=0
-    for i in a:
+    for i in arr:
         if i%2==1:
             c+=1
     print(c//2)        
